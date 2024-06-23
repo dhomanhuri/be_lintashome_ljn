3e56fad4e88843c4e5e001cfa1ccad2680b65285
dhomanhurimtm <dhomanhuri.ilyas@mtm.id>
2024-06-23 22:37:10 +0700

    opand

--- controllers/dataControllers.js
+++ controllers/dataControllers.js
@@ -1,8 +1,8 @@
 const model = require("../models/index");
 const Validator = require("fastest-validator");
 const v = new Validator();
-const moment = require('moment');
-const { Sequelize, Model, DataTypes, Op } = require('sequelize');
+const moment = require("moment");
+const { Sequelize, Model, DataTypes, Op } = require("sequelize");
 const hoststatus = async (req, res) => {
     try {
         let data = await model.HostStatus.findAll({
@@ -12,21 +12,27 @@ const hoststatus = async (req, res) => {
                   (SELECT MAX(id)
                    FROM HostStatuses
                    GROUP BY user)
-                `)
-                }
-            }
-        })
+                `),
+                },
+            },
+        });
         const currentTime = moment();
         for (let index = 0; index < data.length; index++) {
             const element = data[index];
 
-            element.time = moment(element.createdAt).format('HH:mm:ss');
+            element.time = moment(element.createdAt).format("HH:mm:ss");
             const duration = moment.duration(currentTime.diff(element.createdAt));
             const hours = duration.hours();
             const minutes = duration.minutes();
-            element.downtime = hours + ":" + minutes
-            element.count = await model.HostStatus.count({ where: { user: element.user } })
+            element.downtime = hours + ":" + minutes;
+            element.count = await model.HostStatus.count({
+                where: { [Op.and]: [{ user: element.user }, { status: "down" }] },
+            });
+            if (element.status == "up") {
+                delete element;
+            }
         }
+
         res.status(200).send({
             status: true,
             data: data,
@@ -154,8 +160,8 @@ const host_data = async (req, res) => {
     console.log(req.query);
     let data = await model.BulkPop.findOne({
         where: {
-            host: req.query.host
-        }
+            host: req.query.host,
+        },
     });
     console.log(data);
     await model.HostStatus.create({
@@ -164,7 +170,7 @@ const host_data = async (req, res) => {
         pop: data.name,
         status: req.query.status,
     });
-    res.send(req.query)
-}
+    res.send(req.query);
+};
 
 module.exports = { bulkpop, hoststatus, bulkpop_add, bulkpop_put, bulkpop_delete, host_data };
